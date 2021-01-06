3b544abf709c87c61ca181198396c2f30d64131b
Manuel Spadola <Manuel.spadola@stud.fh-campuswien.ac.at>
2021-01-06 18:03:03 +0100

    Version6

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { from } from 'rxjs';
+import { RegisterUserComponent } from './register-user/register-user.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: 'reg',
+    component: RegisterUserComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
